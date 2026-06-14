034867787281fa33ade7a9142d2411c53fded1a9
Kevins Markuss Kanalis <KevinsKanalis@gmail.com>
2026-06-14 21:27:06 +0300

    OK fixed the format

--- src/i18n/lv.ts
+++ src/i18n/lv.ts
@@ -2,7 +2,7 @@ export default {
       "app.title": "Pirmās 30 darba dienās",
 
       "card.1.title": "Iepazītos ar uzņēmumu, produktiem un komandu",
-      "card.1.desc": 'test\n\ntest.\n\ntest \n\ntest\n\n<img src="./assets/test.drawio.png" />\n\ntest\n\ntest\n\ntest',,
+      "card.1.desc": "test\n\ntest.\n\ntest \n\ntest\n\n<img src=\"./assets/test.drawio.png\" />\n\ntest\n\ntest\n\ntest",
       "card.2.title": "Kā apgūtu nepieciešamās tehnoloģijas un procesus",
       "card.2.desc": "",
       "card.3.title": "Praktisku ieguldījumu censtos sniegt pirmajā mēnesī",
